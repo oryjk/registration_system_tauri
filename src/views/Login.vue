e08be17d26dcec1ac3ff26c16d35b2ad7891155d
oryjk <oryjk@qq.com>
2024-10-29 00:24:22 +0800

    1. 整理文件，删掉不需要的文件
    2. 调整默认的窗口大小

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref, inject } from "vue";
+import {inject, ref} from "vue";
 import axios from 'axios'
 
-import { ElNotification } from 'element-plus'
-import { useRouter } from 'vue-router';
+import {ElNotification} from 'element-plus'
+import {useRouter} from 'vue-router';
 
 
 const hostName = inject<string>('hostName');
@@ -11,7 +11,7 @@ const path = inject<string>('path');
 
 
 const form = ref({
-    token: 'MUX9cBF8'
+  token: 'MUX9cBF8'
 })
 
 const isError = ref(true)
@@ -19,62 +19,72 @@ const router = useRouter();
 
 
 interface TokenInfo {
-    token: string,
-    desc: string,
-    valid: boolean
+  token: string,
+  desc: string,
+  valid: boolean
 }
 
 async function login() {
-    console.log(form.value.token)
-    console.log(isError)
-    if (form.value.token.length == 0) {
-        ElNotification({
-            title: '请输入客户端token',
-            message: '一定要输入，否则无法开启信息采集',
-            type: 'error',
-        })
-        return
+  console.log(form.value.token)
+  console.log(isError)
+  if (form.value.token.length == 0) {
+    ElNotification({
+      title: '请输入客户端token',
+      message: '一定要输入，否则无法开启信息采集',
+      type: 'error',
+    })
+    return
+  }
+  let result
+  try {
+    result = await getData(hostName + `${path}/token/check/` + form.value.token)
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      ElNotification({
+        title: '网络错误',
+        message: '请检查服务器是否启动，并检查服务器日志是否有报错',
+        type: 'error',
+      })
+      return
     }
-
-    const result = await getData(hostName + `${path}/token/check/` + form.value.token)
-    if (result.valid) {
-        router.push({ name: 'Working', query: { inviteCode: form.value.token } });
-    }
-    else {
-        ElNotification({
-            title: 'token输入不正确',
-            message: '请输入正确的token',
-            type: 'error',
-        })
-    }
+  }
+  if (!result || result.valid) {
+    router.push({name: 'Working', query: {inviteCode: form.value.token}});
+  } else {
+    ElNotification({
+      title: 'token输入不正确',
+      message: '请输入正确的token',
+      type: 'error',
+    })
+  }
 
 
 }
 
 async function getData(uri: string): Promise<TokenInfo> {
-    return (await axios.get(uri)).data
+  return (await axios.get(uri)).data
 }
 
 </script>
 
 <template>
-    <div class="container">
-        <h1>信息采集</h1>
-        <div class="form_container">
-            <el-input :class="{ 'is-error': isError }" v-model="form.token" placeholder="客户端token:" />
-            <el-button type="primary" @click="login">登录</el-button>
-        </div>
+  <div class="container">
+    <h1>信息采集</h1>
+    <div class="form_container">
+      <el-input :class="{ 'is-error': isError }" v-model="form.token" placeholder="客户端token:"/>
+      <el-button type="primary" @click="login">登录</el-button>
     </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .container {
-    margin: 0;
-    padding-top: 10vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
+  margin: 0;
+  padding-top: 10vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
 
 }
 </style>
